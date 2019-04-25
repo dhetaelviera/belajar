4940ae0bee5a9f1c9145c2af6e8ef5b49d88a09f
dhetaelviera <dhetaindraed@gmail.com>
2019-04-25 14:29:01 -0700

    first

---
+++ app/src/main/java/com/example/myapplication/Constant.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+public class Constant {
+
+    private static final String ROOT_URL="http://192.168.1.10/Android/v1/registerUser.php";
+    public static final String URL_REGISTER=ROOT_URL+"registerUser.php";
+
+
+}
